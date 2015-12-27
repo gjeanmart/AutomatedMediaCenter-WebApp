458c65188d65f920be73f5dc7f63383dbb2ee2be
Grégoire JEANMART <gregoire.jeanmart@gmail.com>
2015-12-27 19:39:06 +0000

    Add scheduler

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,25 @@
 package controllers
 
 
-import play.api._
-import play.api.Play.current
-import play.api.i18n.Messages.Implicits._
-import models._
 import javax.inject.Inject
-import scala.concurrent.Future
-import play.api.mvc._
+
+import actors.BasicActor.SayHello
+import actors._
+import akka.actor._
+import models._
+import play.api.Play.current
+import play.api._
+import play.api.i18n.Messages.Implicits._
 import play.api.libs.ws._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 
-class Application @Inject() (ws: WSClient) extends Controller {
+
+class Application @Inject() (ws: WSClient)(system: ActorSystem)(implicit ec: ExecutionContext)  extends Controller {
 
   // **************************************************
   // ACTIONS
@@ -35,11 +43,8 @@ class Application @Inject() (ws: WSClient) extends Controller {
   def search(phrase: String="", category: String="") = Action.async {
     Logger.debug("ACTION = search(phrase="+phrase+", category="+category+")")
 
-    implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-    val result:Future[TorrentSearchResult] = TorrentSearchResult.search(ws, phrase, category)
-
-    result.map(
+    TorrentSearchResult.search(ws, phrase, category).map(
       response => Ok(views.html.search(response, phrase, category))
     )
   }
@@ -48,11 +53,44 @@ class Application @Inject() (ws: WSClient) extends Controller {
     * poller [ACTION]
     * @return poller.scala.html
     */
-  def poller() = Action { request =>
+  def poller(message:String="") = Action { request =>
     Logger.debug("ACTION = poller")
 
-    Ok(views.html.poller())
+    val basicActor = system.actorOf(BasicActor.props, "hello-actor")
+
+    val cancellable =
+      system.scheduler.schedule(0 milliseconds,
+        1 seconds,
+        basicActor,
+        SayHello(s"message : $message"))
+
+    Thread sleep 60000
+
+    //This cancels further Ticks to be sent
+    cancellable.cancel()
+
+    Ok(views.html.poller(message))
+
+
   }
 
+
+/*
+  implicit val timeout: Timeout = 5.seconds
+
+  def sayHello(name: String) = Action.async {
+    Logger.debug(s"ACTION = sayHello(name=$name)")
+
+
+    val helloActor = system.actorOf(HelloActor.props, "hello-actor")
+
+    (helloActor ? SayHello(name)).mapTo[String].map { message =>
+      Ok(views.html.poller(message))
+    }
+
+
+  }
+*/
+
 }
-            
+
